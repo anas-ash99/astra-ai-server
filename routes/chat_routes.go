253ee2cfa6e7ed16b55d5ade40646f4b96c756e1
Ashraf, Anas <Anas.Ashraf@easy.de>
2024-03-19 22:29:45 +0100

    added home endpoint

--- routes/chat_routes.go
+++ routes/chat_routes.go
@@ -7,6 +7,10 @@ import (
 )
 
 func RegisterUserRoutes(router *mux.Router) {
+
+	router.HandleFunc("/chats", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Welcome to Astra AI Server"))
+	}).Methods(http.MethodGet)
 	router.HandleFunc("/chats", controllers.GetAllChats).Methods(http.MethodGet)
 	router.HandleFunc("/chat", controllers.CreateNewChat).Methods(http.MethodPost)
 
