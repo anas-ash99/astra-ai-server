58b1a32f64370dde3e3e4d5363819d6c68d79dd3
Ashraf, Anas <Anas.Ashraf@easy.de>
2024-03-19 23:26:54 +0100

    edit port number

--- main.go
+++ main.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
 )
 
 func main() {
-	port := ":8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
 	router := mux.NewRouter()
 
 	routes.RegisterUserRoutes(router)
-	err := http.ListenAndServe(port, router)
+	err := http.ListenAndServe("0.0.0.0:"+port, router)
 	if err != nil {
 		fmt.Print("Serer running on port: " + port)
 	}
